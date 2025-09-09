9985d3e7be53a8401bbbb942774416d980ee7b47
shotanaka7 <sho.tanaka1984@me.com>
2025-09-09 16:38:07 +0900

    テストアップ

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700;900&family=Poppins:wght@300;400;500;600;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@700&Noto+Sans+JP:wght@300;400;500;700;900&family=Poppins:wght@300;400;500;600;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
 <script src="<?php echo get_template_directory_uri(); ?>/common/js/jquery.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/common/js/jquery.easing.min.js"></script>
